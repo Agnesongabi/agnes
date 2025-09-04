c0a08473d8ac7cc96af9a20042c6e26f38b87fbf
Agnesongabi <agnesongabi2000@gmail.com>
2025-09-04 08:42:32 +0300

    updated

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('about/resume.html/', views.resume, name='resume'),
+    path('resume/', views.resume, name='resume'),
 
 ]
